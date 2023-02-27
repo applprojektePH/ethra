97cb269e2fae35ea6dbbd4fa70dfbd6d299e235d
sysadmin.aax@fhnw.ch <sysadmin.aax@fhnw.ch>
2023-02-27 18:53:26 +0100

    buf front, email commis

--- src/login.js
+++ src/login.js
@@ -1,5 +1,5 @@
 module.exports = {
     admins: ['alesya.heymann@fhnw.ch'],
-    commission: ['alesya.heymann@gmail.ch', 'alesya.heymann@fhnw.ch', 'alesya.heymann1@fhnw.ch']
+    commission: ['alesya.heymann@fhnw.ch', 'aup.testeraddy@gmail.com']
 }
 
