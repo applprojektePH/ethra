41140b6b1131a213325e8c1a4e6e36582a14060f
sysadmin.aax@fhnw.ch <sysadmin.aax@fhnw.ch>
2023-02-27 18:38:24 +0100

    umlaute, form fields

--- src/routes.js
+++ src/routes.js
@@ -29,6 +29,8 @@ module.exports = function (app, models) {
         {
             destination: './src/public/uploads/',
             filename: function ( req, file, cb ) {
+                file.originalname = Buffer.from(file.originalname, 'latin1').toString(
+                    'utf8');
                 cb( null, file.originalname);
             }
         }
